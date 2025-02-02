4b7f3a0a1850a851459e58e55ca2626f41396698
wackefra <wackefra@outlook.com>
2025-02-02 21:04:44 +0100

    DB in env File

--- sales/admin.py
+++ sales/admin.py
@@ -3,6 +3,6 @@ from .models import SalesActivity
 
 @admin.register(SalesActivity)
 class SalesActivityAdmin(admin.ModelAdmin):
-    list_display = ('activity_date', 'status', 'next_follow_up', 'follow_up_due')
-    search_fields = ('description', 'status')
-    list_filter = ('status', 'activity_date')
+    list_display = ('activity_date', 'status', 'next_follow_up', 'follow_up_due','sales_representative')
+    search_fields = ('description', 'status','sales_representative')
+    list_filter = ('status', 'activity_date','sales_representative__username')
